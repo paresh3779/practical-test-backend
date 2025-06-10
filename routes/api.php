efaa2b572d76859717af0700358a8190b63ffe26
paresh3779 <paresh3779@gmail.com>
2025-06-10 17:17:32 +0530

    User register functionality
    - User register function
    - Setting constant file
    - Adding extra fields for users
    - Setting route for user creation request

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+
+
+Route::group(['namespace' => 'App\Http\Controllers\API'], function() {
+    Route::group(['namespace' => 'V1', 'prefix' => 'v1', 'as' => 'api.v1.'], function() {
+        Route::post("userRegister", "UserController@regitser");
+    });
+});
+
+
